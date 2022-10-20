5b75c61a3b85a3d04c1d541b6d8349342275187a
Bharat Malik <bharatmalik@Bharats-MacBook-Air.local>
2022-10-20 18:24:58 +0530

    This is my first upload

---
+++ Dsasheet.cpp
@@ -0,0 +1,51 @@
+// #include<iostream>
+// #include<string>
+// using namespace std;
+//======Operators======
+// int main(){
+//     int num1=3;
+//     int num2=6;
+
+//     cout<<(num1==num2)<<endl; // false(0)
+//     cout<<(num1!=num2)<<endl; // true(1)
+//     bool exp1=true;
+//     bool exp2=false;
+//     cout<<(exp1||exp2)<<endl; // true
+//     //left and right shift
+//     cout<<(num1<<1)<<endl; // 6
+//     cout<<(num2>>1)<<endl; // 3
+//     cout<<sizeof(num1)<<endl;
+
+//     int a=4;
+//     int b=8;
+//     char name='a';
+
+//     bool flag;
+//     a==name? flag = true : flag= false;
+//     cout<<flag<<endl; //0
+
+    
+// }
+// ========Addition of 2 strings=======
+
+// int main(){
+//     string main="Please  ";
+//     string non="Help";
+//     cout<<main+non<<endl;
+// }
+
+
+//==========Sum of Even NO =========
+// int main(){
+//     int n;
+//     cin>>n;
+//     int i=0;
+//     int sum = 0;
+//     while(i<=n){
+//         sum=sum+i;
+//         i+=2;
+//     }
+//     cout<<"sum of even no: "<<sum<<endl;
+//     return 0;
+// }
+//===========
